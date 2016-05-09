4c03e16a09e6edad31fc11dc2b5cb63c5d0ac87e
Chrys Ugwu <chrys@ghostff.com>
2016-05-09 11:40:43 -0500

    update: dynamics

--- funcs.php
+++ funcs.php
@@ -18,6 +18,12 @@ class find_img{
 		array_pop($colors);
 		return ($type != 'all')? implode(',', $colors) : $colors;
 	}
+	//calcuate image match percentage
+	private static function cal_perc($decrease, $original)
+	{
+		$percentage = $decrease / $original * 100;
+		return round($percentage, 0).'%';
+	}
 	//get diffrents in multiple arrays
 	private static function arr_diff($array1, $array2, $size, $name)
 	{
@@ -31,7 +37,7 @@ class find_img{
 		//if image has no match return
 		if($new_size == 0)
 			return;
-		return array($new_size-$size, $name);
+		return array(self::cal_perc($new_size, $size), $name);
 	}
 	//get the color in each pixel of a specified image
 	//@param 1 image name
@@ -53,37 +59,63 @@ class find_img{
 		return array('pixel' => $new_image, 'size' => $width * $height);
 	}
 	//find images the matches specified images
-	//@param 1 image protoype name
-	//@param 2 path of the image to serch for
-	//@param 3 what to resturn 
+	//@param 1 (string) image protoype name
+	//@param 2 (array)	directory to search for image and they image format to search for
+	//------args
+		//1st element
+			//path/dir to search for image matches
+		//2nd element
+			//image formats to search for in path/dir specified in 1st element
+				//'jpg' searches for match in jpg or jpeg images type alone
+				//'png' searches for match in png images type alone
+				//'gif' searches for match in gif images type alone
+				//'all' searches for match in all images type (jpg, png, gif)
+	//@param 3 (int, null, string) length of matches to return 
 	//---------args	
-		//'all' return all that have atleast one or more matches
-		//'1' return 1 image with highest match
-		//'2' retrun 2 images with more match and more
-	public static function matches($img, $path_to_find, $return)
+		//(int) ----args
+				//if 0 function return nothing
+				//if 1 function returns 1 image with highest match
+				//if 2 or more function retrun 2 or more images with matches(this is random)
+		//(null) ---args
+				//if empty or null function returns all macthes from specified dir
+		//(string)--args
+				//if '100%' function return all image with 100% match
+				//if '68%' function return all image with 68% match
+	public static function matches($img, $pat_nd_fmt, $return = null)
 	{
 		$image_match = array();
 		$img = self::get_total_pixels($img);
 		$i = 0;
-		foreach (glob($path_to_find."/*.png") as $_new_image) {
+		//check match type
+		$pat_nd_fmt[1] = ($pat_nd_fmt[1] == 'all')? glob($pat_nd_fmt[0]."/*.{png,jpg,jpeg,gif}", GLOB_BRACE) : glob($pat_nd_fmt[0]."/*.png");
+		foreach ($pat_nd_fmt[1] as $_new_image) {
 			$new_image = self::get_total_pixels($_new_image);
 			$image_matched = self::arr_diff($img['pixel'], $new_image['pixel'], $img['size'], $_new_image, $return);
-			if($return == 1 && $image_matched[0] == 0){
+			if($return == 1 && $image_matched[0] == '100%'){
 				//return only only one match of image
-				$image_match = $image_matched;
+				$image_match = array($image_matched);
 				break;
 			}
-			else if($return == 'all'){
+			else if($return === null){
 				//return all matches of image
 				$image_match[] = $image_matched;
 			}
-			else if(is_numeric($return)){
+			else if(is_int($return) &&  $return != 0){
 				//return any specified number of image matches
-				$image_match[] = $image_matched[1];
+				$image_match[] = $image_matched;
 				if($i == $return)
 					break;
 				$i++;
-			}	
+			}
+			else if(is_string($return) && $image_matched[0] == '100%'){
+				//return any specified number of image matches
+				$image_match[] = $image_matched;
+				if($i == $return)
+					break;
+				$i++;
+			}
+			else
+				die('invalid arg');
 		}
 		arsort($image_match);
 		return array_filter($image_match);
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
+<style>i{background:rgba(23,168,40,0.5);border:1px solid #17A828;color:#147e21;border-radius:3px;padding:0 5px;}</style>
 <?php
 if(isset($_POST['submit'])){
 	require_once('funcs.php');
-	foreach(find_img::matches($_FILES['selected']['name'], 'find', 3) as $image_matches){
-		var_dump($image_matches);
+	foreach(find_img::matches($_FILES['selected']['name'], array('find', 'all')) as $image_matches){
+		echo '<code> <i>'.$image_matches[1].'</i> is <i>'.$image_matches[0].'</i> matched with uploaded image</code><p />';
 	}
 }
 ?>
 <form method="post" action="index.php" enctype="multipart/form-data">
 	<input type="file" name="selected">
-    <input type="submit" value="uploade" name="submit">
+    <input type="submit" value="upload" name="submit">
 </form>
