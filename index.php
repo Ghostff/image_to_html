d18341e0d6ada1c641efa1c745e2d0debc40846f
ghostff <frankchris@hotmail.com>
2016-05-12 02:41:18 -0500

    update: funcs umini doc

--- index.php
+++ index.php
@@ -21,5 +21,34 @@ echo img_to_htm::render('images/test.png', 4, 4);
 echo img_to_htm::render('images/test.png', null, null. 'string');
 //return rendered images (raw string) with default width and height multiplied by 4
 echo img_to_htm::render('images/test.png', 4, 4, 'string');
+
+
+/*
+******************************Note: acceptable images type: 'png, jpeg and gif'**********************************
+MINI USE
+@1param images name with path
+@2param add attribute to conatiner and child element  (dont change parent and child key name*)
+--------more
+	change div to any html tag if you dont wonna use div as parrent holder (free to modify attribute) NOTE: more attribute more process
+	change span to any html tag if you dont wonna use span as child  (free to modify attribute) NOTE: more attribute more process
+@3param width(leave empty or null to use default width)
+@4param height(leave empty or null to use default height)
+@5param return type ('string' return html character) leave null or empty to exeute returned html
+*/
+require_once('funcs.php');
+$image_attribute = array('parent' => array('div' => array('id' => 'did', 'style' => 'border:1px solid #000;')), //container
+						 'child'  => array('span' => array('class' => 'scl', 'data-id' => 'nw_datat_id'))//child
+						);
+echo img_to_htm::render('any_image.jpg', $image_attribute);
+//return rendered images with default width multiplied by 4
+echo img_to_htm::render('images/test.png', $image_attribute, 4);
+//return rendered images with default height multiplied by 4
+echo img_to_htm::render('images/test.png', $image_attribute, null, 4);
+//return rendered images with default width and height multiplied by 4
+echo img_to_htm::render('images/test.png', $image_attribute, 4, 4);
+//return rendered images (raw string) with default attribute
+echo img_to_htm::render('images/test.png', $image_attribute, null, null. 'string');
+//return rendered images (raw string) with default width and height multiplied by 4
+echo img_to_htm::render('images/test.png', $image_attribute, 4, 4, 'string');
 ?>
 
