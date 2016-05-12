94787744e42a939eaf5afb9a3d375d7931c53635
ghostff <frankchris@hotmail.com>
2016-05-11 19:57:19 -0500

    update: funcs and index DOC

--- index.php
+++ index.php
@@ -9,9 +9,17 @@ MINI USE
 @4param return type ('string' return html character) leave null or empty to exeute returned html
 */
 require_once('funcs.min.php');
-//
-echo img_to_htm::render('test.png');
-//
-echo img_to_htm::render('test.png', 4, 4);
+//return rendered image with default attribute
+echo img_to_htm::render('image/test.png');
+//return rendered image with default width multiplied by 4
+echo img_to_htm::render('image/test.png', 4);
+//return rendered image with default height multiplied by 4
+echo img_to_htm::render('image/test.png', null, 4);
+//return rendered image with default width and height multiplied by 4
+echo img_to_htm::render('image/test.png', 4, 4);
+//return rendered image (raw string) with default attribute
+echo img_to_htm::render('image/test.png', null, null. 'string');
+//return rendered image (raw string) with default width and height multiplied by 4
+echo img_to_htm::render('image/test.png', 4, 4, 'string');
 ?>
 
