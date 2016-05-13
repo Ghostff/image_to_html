aa2ee531f0bddeb8ac63ee7803868536bc31ebab
Chrys Ugwu <chrys@ghostff.com>
2016-05-13 15:57:22 -0500

    update: MEMORY

--- funcs.min.php
+++ funcs.min.php
@@ -30,43 +30,48 @@ class img_to_htm
 	//get the color in each pixel and size of a specified image
 	//@param 1 image name
 	private static function ttl_pxls($img)
-	{
-		$m = $new_image = array();
-		//assign width and height image getimagesize to to $width and $heeight
+	{	
+		$new_image = array();
+		//assign returned array(2) to width and height
 		list($width, $height) = getimagesize($img);
-		//total number of pixels in image
+		
+		//prevent process timing out and maxing out memory
+		if($width*$height > 33760){
+			set_time_limit(0);
+			ini_set('memory_limit', '950M');
+		}
+		//loop through col and rows of pixels to get color output(00, 01, 02, 03, 04...)
 		for($i = 0; $i < $height; $i++)
 			for($k = 0; $k < $width; $k++)
 				$new_image[] = self::image_color($img, $k, $i);
+		//pixel conatains all the colors gotten from pixel while W and H is the image width and height
 		return array('pixel' => $new_image, 'W' => $width, 'H' => $height);
 	}
 	//make image and span have a corresponding with and height
 	private static function align($image_data, $width = null, $height = null)
 	{
-		//set default property for height, width and oppcaity(when arg is null)
+		//set default property for height, width (for null args)
 		if(!$width) $width = 1;
 		if(!$height) $height = 1;
 		
-		$img_color 	= $image_data['pixel'];
-		$img_height = $image_data['H'];
-		$img_width 	= $image_data['W'];
-
+		$img_color 	= $image_data['pixel'];//array of all colors found in each pixel
+		$img_height = $image_data['H'];//int of image height
+		$img_width 	= $image_data['W'];//int of image width
+		
 		$parent = '<div id="dHksl">';
 		$child	= '<i style="';
 		$HTML = '<style>#dHksl{width:'.$img_width*$width.'px;height:'.$img_height*$height.'px;}#dHksl i{width:'.$width.'px;height:'.$height.'px;float:left;}</style>';
 		$HTML .= $parent;
 		foreach($img_color as $key => $pxl_color)
 			$HTML .= $child.'background:#'.$pxl_color.';"></i>';
-		return($HTML);
+		return($HTML.'</div>');
 	}
 	public static function render($img_name, $width = null, $height = null, $type = null)
 	{
-		//debuger remove this line (if image is always specified)
+		//debuger: remove this line (if image is always specified)
 		if(!file_exists($img_name))
 			return 'invalid image';
 			
-		//prevent process from timing out
-		set_time_limit(0);
 		if($type == 'string')
 			return htmlspecialchars(self::align(self::ttl_pxls($img_name),  $width, $height));
 		else 
